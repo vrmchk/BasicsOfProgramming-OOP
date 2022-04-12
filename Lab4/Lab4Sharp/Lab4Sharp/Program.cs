cb8cbac697d6b6980023bbbc8ea932fe3bd0de87
vrmchk <ihorveremchuk8@gmail.com>
2022-04-13 01:24:06 +0300

    task execution in main

--- Lab4/Lab4Sharp/Lab4Sharp/Program.cs
+++ Lab4/Lab4Sharp/Lab4Sharp/Program.cs
@@ -9,8 +9,28 @@ namespace Lab4Sharp
     {
         static void Main(string[] args)
         {
-            TimeWorker timeWorker = new TimeWorker();
-            timeWorker.Start();
+            Time[] timeArray =
+            {
+                new Time(18),
+                new Time(14, 30, 40),
+                new Time(3, 25)
+            };
+            
+            TimeWorker.PrintTimeArray(timeArray, "Starting time");
+
+            timeArray[0] += 17;
+            timeArray[1] += 34;
+
+            TimeWorker.PrintTimeArray(timeArray, "Changed time");
+
+            Console.WriteLine("Difference between time 1 and 2");
+            Time timeDifference = timeArray[0] - timeArray[1];
+            timeDifference.Print();
+
+            Console.WriteLine("\nTime from time 3 to the end of the day");
+            timeArray[2].GetTimeToEndOfDay().Print();
+            Console.ReadLine();
+
         }
     }
 }
--- Lab4/Lab4Sharp/Lab4Sharp/TimeWorker.cs
+++ Lab4/Lab4Sharp/Lab4Sharp/TimeWorker.cs
@@ -5,41 +5,16 @@ using System.Linq;
 
 namespace Lab4Sharp
 {
-    public class TimeWorker
+    public static class TimeWorker
     {
-        public void Start() //запуск роботи з часом
-        {
-            Time[] timeArray =
-            {
-                new Time(18),
-                new Time(14, 30, 40),
-                new Time(3, 25)
-            };
-            
-            PrintTimeArray(timeArray, "Starting time");
-
-            timeArray[0] += 17;
-            timeArray[1] += 34;
-
-            PrintTimeArray(timeArray, "Changed time");
-
-            Console.WriteLine("Difference between time 1 and 2");
-            Time timeDifference = timeArray[0] - timeArray[1];
-            timeDifference.Print();
-
-            Console.WriteLine("\nTime from time 3 to the end of the day");
-            timeArray[2].GetTimeToEndOfDay().Print();
-            Console.ReadLine();
-        }
-
-        private void PrintTimeArray(Time[] timeArray, string prePrint = "") //вивести масив часу
+        public static void PrintTimeArray(Time[] timeArray, string prePrint = "") //вивести масив часу
         {
             Console.WriteLine(prePrint);
             Array.ForEach(timeArray, t => t.Print());
             Console.WriteLine();
         }
 
-        private Time[] ConsoleArrayOfTimes(int size) //масив часу з даними з консолі
+        public static Time[] ConsoleArrayOfTimes(int size) //масив часу з даними з консолі
         {
             var timeArray = new Time[size];
             for (int i = 0; i < size; i++)
